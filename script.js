94d68b2879c11982a5b64a92ebcfde44ebd17db4
fatxullo <fathullozayniddinov@gmail.com>
2021-06-08 14:31:56 +0500

    hamburger-menu

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ AOS.init({
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
-  delay: 1, // values from 0 to 3000, with step 50ms
+  delay: 250, // values from 0 to 3000, with step 50ms
   duration: 400, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
@@ -24,3 +24,8 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+function menuBtn(){
+  document.getElementById("responsive-menu").style.top="75px";
+
+}
